96a615c32cee1df6834102b5066967b13396003e
yang chen <yangchen@dhcp-10-29-221-45.dyn.MIT.EDU>
2023-12-03 10:54:02 -0500

    Add ethanol reaction mech

--- DropEvapSimp.py
+++ DropEvapSimp.py
@@ -2,13 +2,4 @@ import numpy as np
 import cantera as ct
 from scipy.optimize import fsolve
 #DesignParameters
-Tinf = 1000#K
-Pref = 201325#Pa
-Yvinf = 0#
-rd = 100*1e-6 #50 micron
-rinf = rd*10 #1cm
-numPt = 100
-LHVv = 335*1e3 #J/kg
-hfg = 256e3 #J/kg
-Psatref = 101325 #Pa
-Tsatref = 489.45 #K
+YFuel = 
