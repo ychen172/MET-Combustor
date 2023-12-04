84e3ae03941989d6161b564ea0818b84f88bf368
yang chen <yangchen@dhcp-10-29-221-45.dyn.MIT.EDU>
2023-12-04 11:15:16 -0500

    Update to test out differen thrust requirements

--- DropEvapCom.py
+++ DropEvapCom.py
@@ -6,8 +6,6 @@ import pandas as pd
 YOxi = "O2:0.2314,N2:0.7622,H2O:0.0064"
 YFue = "C2H5OH:1"
 ReacMech = 'CRECK2019-NOx-NoBin.yaml'
-Pref = 485501 #Pa
-Tinf = 487.233 #K
 Tdrop = 298 #K Droplet Temperature
 YOxiInf = 1.0 #Farfield Composition
 rhol = 789 #kg/m3
@@ -25,12 +23,16 @@ CycleName = "ACSRQLDerived_DualFine_EthaEthaOutPutTot.csv"
 Cycle = pd.read_csv(CycleName)
 TCycleLst = Cycle['Tt3[K]'].values[:]
 PCycleLst = Cycle['Pt3[Pa]'].values[:]
+PrefLst = PCycleLst[:] #485501 #Pa
+TinfLst = TCycleLst[:] #487.233 #K
 #Test Conditions
-rdIniList = [50e-6,30e-6,10e-6] #m radius of droplet
+rdIniList = [50e-6,15e-6] #m radius of droplet
 #Solve
 Result = []
 for i in range(len(rdIniList)):
-    Result.append(EvapCalc(YOxi,YFue,ReacMech,Pref,Tinf,Tdrop,YOxiInf,rdIniList[i],rhol,cpl,LHVapor,LHVheat,FAst,TsatRef,PsatRef,MachLiner,Gamma,fracMassEvap))
+    Result.append([])
+    for j in range(len(TinfLst)):
+        Result[i].append(EvapCalc(YOxi,YFue,ReacMech,PrefLst[j],TinfLst[j],Tdrop,YOxiInf,rdIniList[i],rhol,cpl,LHVapor,LHVheat,FAst,TsatRef,PsatRef,MachLiner,Gamma,fracMassEvap))
 """
 rdLst = Result[0]
 time = Result[1]
@@ -49,10 +51,10 @@ YSLst = Result[11]
 fig = plt.figure(dpi = 300)
 ax  = fig.add_subplot(1,1,1)
 for i in range(len(rdIniList)):
-    plt.plot(Result[i][1]*1e3, (Result[i][0]*1e6)**2)
+    plt.plot(Result[i][0][1]*1e3, (Result[i][0][0]*1e6)**2)
 plt.ylabel('Square of Droplet Radius [um2]')
 plt.xlabel('Time [ms]')
-plt.title('Relative Mach Number '+str(MachLiner))
+plt.title('Relative Mach Number '+str(MachLiner)+' Tinf: '+str(np.round(TinfLst[0],3))+' K Pinf: '+str(np.round(PrefLst[j],3))+' Pa')
 plt.grid(True)
 plt.savefig("rdSquare.jpg")
 
@@ -60,21 +62,27 @@ plt.savefig("rdSquare.jpg")
 fig = plt.figure(dpi = 300)
 ax  = fig.add_subplot(1,1,1)
 for i in range(len(rdIniList)):
-    plt.plot(Result[i][1][:-1]*1e3, Result[i][9][:-1], label="rd: "+str(rdIniList[i]*1e6)+" um")
+    plt.plot(Result[i][0][1][:-1]*1e3, Result[i][0][9][:-1], label="rd: "+str(rdIniList[i]*1e6)+" um")
 plt.ylabel('Flame Temperature [K]')
-plt.ylim([np.mean(Result[i][9][:-1])-5,np.mean(Result[i][9][:-1])+5])
 plt.xlabel('Time [ms]')
-plt.title('Relative Mach Number '+str(MachLiner))
+plt.legend()
+plt.title('Relative Mach Number '+str(MachLiner)+' Tinf: '+str(np.round(TinfLst[0],3))+' K Pinf: '+str(np.round(PrefLst[j],3))+' Pa')
 plt.grid(True)
 plt.savefig("TFlame.jpg")
 
-#Plot Droplet LifeTime
+#Combined Droplet Lifetime
+DropLifeTime = []
+for i in range(len(rdIniList)):
+    DropLifeTime.append([])
+    for j in range(len(TinfLst)):
+        DropLifeTime[i].append(Result[i][j][1][-1])
 fig = plt.figure(dpi = 300)
 ax  = fig.add_subplot(1,1,1)
 for i in range(len(rdIniList)):
-    plt.plot(rdIniList[i]*1e6,Result[i][1][-1]*1e3,'+')
+    plt.plot(TinfLst, np.array(DropLifeTime[i])*1e3,label="rd: "+str(rdIniList[i]*1e6)+" um")
 plt.ylabel('Droplet Lifetime [ms]')
-plt.xlabel('Initial Droplet Radius [um]')
+plt.xlabel('Ambient Temperature [K]')
+plt.legend()
 plt.title('Relative Mach Number '+str(MachLiner))
 plt.grid(True)
 plt.savefig("Droplife.jpg")
